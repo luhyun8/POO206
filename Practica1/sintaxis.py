229071ca88c2b97a2550e0e53dcb414e5a2dde73
luhyun8 <4423585629lm@gmail.com>
2025-05-16 18:19:09 -0600

    variables

--- Practica1/sintaxis.py
+++ Practica1/sintaxis.py
@@ -4,3 +4,28 @@
 
 """ aqui va un comentario 
 de todas las lineas """
+
+# 2. strings
+print("Hola")
+print('Hola de nuevo')
+
+variable1= "hola soy una cadena"
+print(len(variable1))
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3. Variables
+x="guadalupe"
+x= 7
+x = 7.7
+print(x)
+
+x= int(3)
+y= float(3)
+z= str(3)
+
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
