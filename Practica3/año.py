c5bf9b89f926d8fc465e95f3e4c24e1a28048dec
luhyun8 <4423585629lm@gmail.com>
2025-05-23 09:19:07 -0600

    año

---
+++ Practica3/año.py
@@ -0,0 +1,9 @@
+try:
+        año = int(input("Introduce un año: "))
+        if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+            print(f"El año {año} es bisiesto.")
+        else:
+            print(f"El año {año} no es bisiesto.")
+except ValueError:
+        print("Error: Debes introducir un año válido.")
+
