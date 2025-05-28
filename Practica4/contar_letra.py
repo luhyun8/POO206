79768822491e370b25da847667b1c61c5956e6e7
luhyun8 <4423585629lm@gmail.com>
2025-05-28 12:56:37 -0600

    contar letra

---
+++ Practica4/contar_letra.py
@@ -0,0 +1,11 @@
+try:
+    frase= input("Ingrese una frase:")
+    letra = input("Ingrese una letra:")
+    if len(letra) != 1:
+        raise ValueError("Debes introducir solo una letra.")
+    contador = frase.count(letra)
+    print(f"La letra '{letra}' aparece {contador} veces en la frase.")
+except ValueError as e:
+        print("Error:", e)
+except Exception as e:
+        print("Se produjo un error inesperado:", e)
