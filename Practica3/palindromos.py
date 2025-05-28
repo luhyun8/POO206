8a20c4d0755f1a7a2e87846632d96b07b68d9c31
luhyun8 <4423585629lm@gmail.com>
2025-05-28 09:50:47 -0600

    palindromos

--- Practica3/palindromos.py
+++ Practica3/palindromos.py
@@ -1,3 +1,8 @@
 try:
-    cadena = int (input"Ingresa una cadena:")
-    if cadena 00
+    cadena = input ("Ingresa una cadena de caracteres:")
+    if cadena == cadena[::-1]:
+        print(f"La cadena '{cadena}' es palíndroma")
+    else:
+        print(f"La cadena '{cadena}' no es palíndroma")
+except Exception as e :
+    print(f"Ocurrio un error: {e}")
