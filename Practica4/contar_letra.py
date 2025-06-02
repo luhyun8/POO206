9ba456f3f029911b0a605d96f9e8df0281bc6a6a
luhyun8 <4423585629lm@gmail.com>
2025-06-01 21:44:41 -0600

    Update contar_letra.py

--- Practica4/contar_letra.py
+++ Practica4/contar_letra.py
@@ -4,6 +4,7 @@ try:
     if len(letra) != 1:
         raise ValueError("Debes introducir solo una letra.")
     contador = frase.count(letra)
+    if any (char,i)
     print(f"La letra '{letra}' aparece {contador} veces en la frase.")
 except ValueError as e:
         print("Error:", e)
