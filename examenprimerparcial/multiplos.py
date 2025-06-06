6001447ad8810dbe67de18a120e9b51a95db41ce
luhyun8 <4423585629lm@gmail.com>
2025-06-06 09:05:12 -0600

    examen

--- examenprimerparcial/multiplos.py
+++ examenprimerparcial/multiplos.py
@@ -1,8 +1,7 @@
-#programa que visualice numeros multiplos de cinco comprendidos entre diez y doscientos
 try:
     for i in range(10, 201):
         if i % 5 == 0:
             print(i)
 except ValueError as e:
-    print("Error.Debe ser un numero multiplo de 5",e)
+    print("Error",e)
         
