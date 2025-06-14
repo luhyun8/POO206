2fae626fa2cb1b04a85775c1f3c991632ec524bd
luhyun8 <4423585629lm@gmail.com>
2025-06-13 18:41:39 -0600

    uso de mysql

--- FlaskProject/app.py
+++ FlaskProject/app.py
@@ -1,5 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
+import MySQLdb
+
 app = Flask (__name__)
+
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']="Jinnie320"
+app.config['MYSQL_DB']="dbflask"
+# app.config['MYSQL_PORT']="3306" //se puede usar en caso de cambio de puerto
+
+mysql = MySQL(app)
+
+#RUTA para probar la coneccion a MYSQL
+@app.route('/DBCheck')
+def DBChek():
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('Select 1')
+        return jsonify({'status':'ok','message':'Conexión exitosa'}),200
+    except MySQLdb.MySQLError as e:
+        return jsonify({'status':'Error','message':str(e)}),500
+
+
 
 #RUTA SIMPLE
 @app.route('/')
